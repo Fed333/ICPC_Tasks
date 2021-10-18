2e43d11c3891cf8039d8043846a2f3b80ca593a5
Fed333 <kovalchuk.roman03@gmail.com>
2021-10-18 16:06:00 +0300

    solved 1534A problem

--- 1534A/src/main.cpp
+++ 1534A/src/main.cpp
@@ -8,76 +8,6 @@ using namespace std;
 typedef vector<char> vc;
 typedef vector<vc> vvc;
 
-#ifdef DEBUG
-int main() {
-
-	int t;
-	scanf("%d", &t);
-	getchar();
-	while (t > 0) {
-		int n, m;
-		scanf("%d %d", &n, &m);
-		getchar();
-
-		vvc flag(n+2, vc(m, '.'));
-		for (int i = 1; i < n+1; ++i) {
-			for (int j = 0; j < m; ++j) {
-				char symb;
-				scanf("%c", &symb);
-				flag[i][j] = symb;
-			}
-			getchar();
-		}
-		
-		bool paint = true;
-		for (int i = 1; i < n+1 && paint; ++i) {
-
-			for (int j = 1; j < m - 1; ++j) {
-				char s = flag[i][j];
-				if (s == 'R') {
-					if (flag[i][j - 1] != 'R' && flag[i][j + 1] != 'R' && flag[i + 1][j] != 'R' && flag[i-1][j] != 'R') {
-						flag[i][j - 1] = flag[i][j + 1] = flag[i + 1][j] = flag[i-1][j] = 'W';
-					}
-					else {
-						paint = false;
-					}
-				}
-				else if (s == 'W') {
-					if (flag[i][j - 1] != 'W' && flag[i][j + 1] != 'W' && flag[i + 1][j] != 'W' && flag[i-1][j] != 'W') {
-						flag[i][j - 1] = flag[i][j + 1] = flag[i + 1][j] = flag[i-1][j] = 'R';
-					}
-					else {
-						paint = false;
-					}
-				}
-				else {
-					flag[i][j] = (flag[i][j - 1] == 'R') ? 'W' : 'R';
-					flag[i][j+1] = (flag[i][j] == 'R') ? 'W' : 'R';
-				}	
-			}
-
-		}
-
-		if (paint) {
-			printf("YES\n");
-			for (int i = 1; i < n+1; ++i) {
-				for (int j = 0; j < m; ++j) {
-					printf("%c", flag[i][j]);
-				}
-				printf("\n");
-			}
-		}
-		else {
-			printf("NO\n");
-		}
-
-		--t;
-	}
-
-	return 0;
-}
-#endif
-
 int main() {
 	int t;
 	scanf("%d", &t);
@@ -99,7 +29,7 @@ int main() {
 		}
 
 
-		int pos[][2] = { {0, 1}, {1, 0} };	//0 - äëÿ R, 1 - äëÿ L
+		int pos[][2] = { {0, 1}, {1, 0} };	//0 - Ð´Ð»Ñ R, 1 - Ð´Ð»Ñ L
 		bool established = false;
 		bool paint = true;
 		for (int i = 0; i < n; ++i) {
@@ -143,7 +73,7 @@ int main() {
 				for (int j = 0; j < m; ++j) {
 					char s;
 					if (j%2 == 0) {
-						//ÿêèé ñèìâîë äðóêóâàòè íà ì³ñö³ ïàðíîãî ³íäåêñà ðÿäêà i
+						//ÑÐºÐ¸Ð¹ ÑÐ¸Ð¼Ð²Ð¾Ð» Ð´Ñ€ÑƒÐºÑƒÐ²Ð°Ñ‚Ð¸ Ð½Ð° Ð¼Ñ–ÑÑ†Ñ– Ð¿Ð°Ñ€Ð½Ð¾Ð³Ð¾ Ñ–Ð½Ð´ÐµÐºÑÐ° Ñ€ÑÐ´ÐºÐ° i
 						s = pos[0][parity_row] == 0 ? 'R' : 'W';
 					}
 					else {
@@ -158,12 +88,6 @@ int main() {
 			printf("NO\n");
 		}
 
-		//for (int i = 0; i < 2; ++i) {
-		//	for (int j = 0; j < 2; ++j) {
-		//		printf("%d ", pos[i][j]);
-		//	}
-		//	printf("\n");
-		//}
 		--t;
 	}
 
