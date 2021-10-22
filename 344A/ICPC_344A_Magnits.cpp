17625a81e15ebc85e00e692b795d4b4c77f68907
Fed333 <kovalchuk.roman03@gmail.com>
2021-10-22 12:42:42 +0300

    qwe

---
+++ 344A/ICPC_344A_Magnits.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    vector<int> arr;
+
+    for (int i = 0; i < a; i++)
+    {
+        int b;
+        cin >> b;
+        arr.push_back(b);
+
+    }
+
+    int num=0;
+    int last=arr[0];
+    for (int i = 1; i < a; i++)
+    {
+            if (last%10==arr[i]/10)
+            {
+                num++;
+            }
+
+            last = arr[i];
+    }
+    num++;
+    cout<<num;
+}
